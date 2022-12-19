1035e6b2079d4037ab43570d645db82f969a1849
Kadir Engin <kadirengin624@hotmail.com>
2022-12-19 17:28:27 +0300

    start activation functios (ReLU).

---
+++ ReLU.py
@@ -0,0 +1,39 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+#np.random.seed(0)
+""" 
+X = [[1, 2, 3, 2.5],
+     [2, 5, -1, 2],
+     [-1.5, 2.7, 3.3, -0.8]]
+ """
+X, y = spiral_data(100, 3)
+
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
+class Layer_Dense:
+    #n_inputs is like number of features
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        #creates zeroes for biases
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        #do the dot product which is (x * y + z * a)
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        # append bigger one from 0 and inputs
+        self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+
+activation1.forward(layer1.output)
+
+print(activation1.output)
